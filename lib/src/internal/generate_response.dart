e7656e1df003ea29e6d6deb8a8b95c227df016a3
Kevin Segaud <me@kleak.dev>
2023-04-22 12:14:26 +0200

    better management of accept headers

--- lib/src/internal/generate_response.dart
+++ lib/src/internal/generate_response.dart
@@ -6,29 +6,55 @@ import 'package:shelf_helpers/src/internal/network_object.dart';
 Response generateResponse(final Request request, final NetworkObject object,
     {final int status = HttpStatus.ok,
     final String defaultContentType = 'application/json'}) {
-  var acceptHeader = ContentType.parse(
-      request.headers[HttpHeaders.acceptHeader] ?? defaultContentType);
-  if (request.headers[HttpHeaders.acceptHeader] == '*/*') {
-    acceptHeader = ContentType.parse(defaultContentType);
-  }
-  if (acceptHeader.primaryType == 'application') {
-    if (acceptHeader.subType == 'json' && object is JsonNetworkObject) {
-      return Response(
-        status,
-        body: object.toJsonString(),
-        headers: {
-          HttpHeaders.contentTypeHeader: 'application/json',
-        },
-      );
-    } else if (acceptHeader.subType == 'xml' && object is XmlNetworkObject) {
-      return Response(
-        status,
-        body: object.toXmlString(),
-        headers: {
-          HttpHeaders.contentTypeHeader: 'application/xml',
-        },
-      );
+  final requestAccept =
+      request.headers[HttpHeaders.acceptHeader] ?? defaultContentType;
+  final acceptAll = requestAccept.contains('*/*');
+
+  if (acceptAll) {
+    if (object is JsonNetworkObject) {
+      return _jsonResponse(status, object);
+    } else if (object is XmlNetworkObject) {
+      return _xmlResponse(status, object);
     }
   }
+
+  final splitRequestAccept = requestAccept.split(';');
+  final filteredAccept =
+      splitRequestAccept.where((element) => !element.contains('=')).toList();
+  final acceptableHeader =
+      filteredAccept.map((e) => ContentType.parse(e)).toList();
+
+  for (final acceptHeader in acceptableHeader) {
+    if (acceptHeader.primaryType == 'application' &&
+        acceptHeader.subType == 'json' &&
+        object is JsonNetworkObject) {
+      return _jsonResponse(status, object);
+    } else if (acceptHeader.primaryType == 'application' &&
+        acceptHeader.subType == 'xml' &&
+        object is XmlNetworkObject) {
+      return _xmlResponse(status, object);
+    }
+  }
+
   return Response.badRequest();
+}
+
+Response _jsonResponse(int status, JsonNetworkObject object) {
+  return Response(
+    status,
+    body: object.toJsonString(),
+    headers: {
+      HttpHeaders.contentTypeHeader: 'application/json',
+    },
+  );
+}
+
+Response _xmlResponse(int status, XmlNetworkObject object) {
+  return Response(
+    status,
+    body: object.toXmlString(),
+    headers: {
+      HttpHeaders.contentTypeHeader: 'application/json',
+    },
+  );
 }
