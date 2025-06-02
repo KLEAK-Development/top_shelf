47d3223b33201be69f768683eeb705880a7a36bb
kevin <kevin@sakemaer.fr>
2025-06-02 17:20:15 +0200

    fix format

--- lib/src/services/authentication/routes/refresh/refresh.dart
+++ lib/src/services/authentication/routes/refresh/refresh.dart
@@ -7,7 +7,7 @@ import 'package:top_shelf/src/services/common/services/account/account_service_i
 Future<Tokens> handler(Request request) async {
   final refreshToken = request.get<JsonWebToken>();
   final service = request.get<AccountServiceInterface>();
-  
+
   // Use the AccountService to handle token refresh logic
   return await service.refreshTokens(refreshToken.jwt);
 }
